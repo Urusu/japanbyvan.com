7f59aa556f0ebd621aee935f8f45cc54b8bd95c1
Urusu Kuma <43748916+Urusu@users.noreply.github.com>
2019-11-22 23:50:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -146,13 +146,13 @@ function initMap() {
     lat: 34.747572,
     lng: 135.509663
   };
-  var test = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
     center: depot
   });
   var marker = new google.maps.Marker({
     position: depot,
-    map: test
+    map: map
   });
 }
 

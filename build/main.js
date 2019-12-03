954a8086ea017f4ed0b1ed6ac60dd97aa0089127
Urusu Kuma <43748916+Urusu@users.noreply.github.com>
2019-12-03 21:35:10 +0900

    Update main.js

--- build/main.js
+++ build/main.js
@@ -151,10 +151,9 @@ function initMap() {
     center: depot
   });
   var marker = new google.maps.Marker({
-    position: depot,
     map: map,
     animation: google.maps.Animation.DROP,
-    title: "JapanByVan"
+    position: depot
   });
 }
 
